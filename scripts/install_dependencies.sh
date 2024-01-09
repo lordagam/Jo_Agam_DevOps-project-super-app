e309bcfa5cdd04f6dd844a9dac2a01c8a03ddb5b
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-09 23:39:41 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sudo yum install docker -y
-sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin
-sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
+sudo apt-get purge -y docker docker.io
+sudo apt-get autoremove -y --purge  docker docker.io docker-ce 
 #curl -fsSL https://get.docker.com/ | sh
 #sudo systemctl start docker
 #sudo systemctl enable docker
