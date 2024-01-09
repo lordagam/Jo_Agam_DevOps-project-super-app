c80ccdd360ba3210a3c0a1e5b1d9eba5b70dbe3c
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-09 23:54:22 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #sudo systemctl start docker
 #sudo systemctl enable docker
-git clone https://github.com/lordagam/Jo_Agam_DevOps-project-super-app.git
+sudo git clone https://github.com/lordagam/Jo_Agam_DevOps-project-super-app.git
 cd Jo_Agam_DevOps-project-super-app
-docker compose up -d
+sudo docker compose up -d
