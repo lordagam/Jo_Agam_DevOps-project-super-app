c364df4a15ae70a3c7a6dd5ab70c8c73a7f89787
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-10 00:51:59 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,5 +3,5 @@
 #sudo systemctl enable docker
 #sudo yum install git -y
 sudo git clone https://github.com/lordagam/Jo_Agam_DevOps-project-super-app.git
-cd Jo_Agam_DevOps-project-super-app
+cd Jo_Agam_DevOps-project-super-app/
 sudo docker-compose up -d
