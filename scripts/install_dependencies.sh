d19f84ca8a61905dd03d635776d89cdf6c8a5bce
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-09 23:20:43 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #sudo yum install docker -y
-curl -fsSL https://get.docker.com/ | sh
+#curl -fsSL https://get.docker.com/ | sh
+sudo apt install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
