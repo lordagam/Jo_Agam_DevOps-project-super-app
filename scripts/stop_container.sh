ed172ab637f9e1cfca6d9a90974a917ce1fa8d26
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-12 09:59:35 +0200

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-docker compose down
+docker stop lordagam/joseph-agam-project-devops_node:latest
+docker stop lordagam/joseph-agam-project-devops_php:latest
+docker stop lordagam/joseph-agam-project-devops_mysql:8.0.28
+
+docker rm lordagam/joseph-agam-project-devops_node:latest
+docker rm lordagam/joseph-agam-project-devops_php:latest
+docker rm lordagam/joseph-agam-project-devops_mysql:8.0.28
+
+
+
+
+
