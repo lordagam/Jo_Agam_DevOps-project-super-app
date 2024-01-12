baed7e58c6f7bdb91b93f0116c496f585361fd56
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-12 10:22:37 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #sudo git clone https://github.com/lordagam/Jo_Agam_DevOps-project-super-app.git
 #cd Jo_Agam_DevOps-project-super-app/
-docker run -d --name super-app-node -p 3000:3000 lordagam/joseph-agam-project-devops_node
-docker run -d --name super-app-php -p 80:80 lordagam/joseph-agam-project-devops_php
+docker run -d --name super-app-node -p 3000:3000 lordagam/joseph-agam-project-devops_node:latest
+docker run -d --name super-app-php -p 80:80 lordagam/joseph-agam-project-devops_php:latest
 docker run -d --name super-app-mysql -p 3306:3306 -e MYSQL_DATABASE=super-app -e MYSQL_ROOT_PASSWORD=password mysql:8.0.28
