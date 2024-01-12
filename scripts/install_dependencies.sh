2589494ecac68859bf2b94a8f4710d0a6eede7e4
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-12 13:07:08 +0200

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,6 @@ curl -fsSL https://get.docker.com/ | sh
 sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
-docker pull lordagam/joseph-agam-project-devops_node:latest
-docker pull lordagam/joseph-agam-project-devops_php:latest
+docker pull lordagam/joseph-agam-project-devops_node:22
+docker pull lordagam/joseph-agam-project-devops_php:22
 docker pull lordagam/joseph-agam-project-devops_mysql:8.0.28
