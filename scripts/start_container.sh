bd7dfba59021cd522ba24b3731a2e43d302c2236
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-12 13:18:33 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-#sudo git clone https://github.com/lordagam/Jo_Agam_DevOps-project-super-app.git
-#cd Jo_Agam_DevOps-project-super-app/
-#docker pull lordagam/joseph-agam-project-devops_node:latest
-#docker pull lordagam/joseph-agam-project-devops_php:latest
-#docker pull lordagam/joseph-agam-project-devops_mysql:8.0.28
 
 docker run -itd --name joseph-agam-project-devops_node -p 3000:3000 lordagam/joseph-agam-project-devops_node:22
 docker run -itd --name joseph-agam-project-devops_php -p 80:80 lordagam/joseph-agam-project-devops_php:22
